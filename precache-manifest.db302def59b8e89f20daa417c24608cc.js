108929730ba639b85724b1c5fe46605d7cd3ce25
Maxim Boichenko <maxim.boichenko@gmail.com>
2019-11-08 07:18:38 +0300

    init commit

---
+++ precache-manifest.db302def59b8e89f20daa417c24608cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "004d817ca0b5bc70e09d9dd0a9853433",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43fc238babc87e7926a1",
+    "url": "/static/css/main.2ac50809.chunk.css"
+  },
+  {
+    "revision": "d642b515bacd3020e990",
+    "url": "/static/js/2.bc560996.chunk.js"
+  },
+  {
+    "revision": "43fc238babc87e7926a1",
+    "url": "/static/js/main.d8499642.chunk.js"
+  },
+  {
+    "revision": "11bb0e49291e53ea25db",
+    "url": "/static/js/runtime-main.55cff43a.js"
+  }
+]);
