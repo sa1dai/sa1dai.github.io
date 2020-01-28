21276366509e124ee516c214c35e6cdc687146d2
sa1dai <maxim.boichenko@gmail.com>
2020-01-28 23:48:23 +0300

    tfs-coursework-client added

---
+++ tfs-coursework-client/precache-manifest.0c2c11af727321ce682a84f12a798bda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e4ea791c97dbc0bbf421",
+    "url": "/static/js/main.d8b2cbc0.chunk.js"
+  },
+  {
+    "revision": "bf4c394c0a667dc42761",
+    "url": "/static/js/2.d2083d69.chunk.js"
+  },
+  {
+    "revision": "e4ea791c97dbc0bbf421",
+    "url": "/static/css/main.4f471f49.chunk.css"
+  },
+  {
+    "revision": "da7b3a856120007e8af6f7993504d9fc",
+    "url": "/index.html"
+  }
+];
