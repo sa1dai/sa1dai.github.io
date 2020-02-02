5477b2506d1fd244bb826ec27551a2d1389ca65c
sa1dai <maxim.boichenko@gmail.com>
2020-02-03 01:32:41 +0300

    Add files via upload

---
+++ project-zero-admin-panel-2/precache-manifest.7b446668cfdb9793f4fe9018aff97482.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcdb9dd6df77f19695c6c1046d73135f",
+    "url": "/project-zero-admin-panel-2/index.html"
+  },
+  {
+    "revision": "3a0cd932d320d6c772f9",
+    "url": "/project-zero-admin-panel-2/static/css/main.90641b45.chunk.css"
+  },
+  {
+    "revision": "2455e84ac50fa0900a88",
+    "url": "/project-zero-admin-panel-2/static/js/2.c83b9316.chunk.js"
+  },
+  {
+    "revision": "3a0cd932d320d6c772f9",
+    "url": "/project-zero-admin-panel-2/static/js/main.306e1ba6.chunk.js"
+  },
+  {
+    "revision": "18b3a64a85c6058c63f5",
+    "url": "/project-zero-admin-panel-2/static/js/runtime-main.d9594c30.js"
+  }
+]);
